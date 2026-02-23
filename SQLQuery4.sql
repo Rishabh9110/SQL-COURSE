1a799e661114420b664bb35f1e2c3e8c990d18a1
Rishabh9110 <rrishabh283@gmail.com>
2026-02-23 15:14:00 +0530

    UPDATING QUERY LIST

---
+++ SQLQuery4.sql
@@ -0,0 +1,3 @@
+--the where clause is used when we want to filter our data based on the condition
+select * from customers
+select * from customers where score>500
---
+++ SQLQuery5.sql
@@ -0,0 +1,3 @@
+--retrieve customer with a score not equal to 0
+select * from customers
+select * from customers where score!=0
